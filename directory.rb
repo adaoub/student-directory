5216760c20cd58106572e09b66e774e1b800041a
Abdullah <adaoub0@gmail.com>
2022-08-10 11:37:43 +0100

    Added ability to add students by the user

--- directory.rb
+++ directory.rb
@@ -1,25 +1,42 @@
-students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates",
-]
+# students = [
+#   { name: "Dr. Hannibal Lecter", cohort: :november },
+#   { name: "Darth Vader", cohort: :november },
+#   { name: "Nurse Ratched", cohort: :november },
+#   { name: "Michael Corleone", cohort: :november },
+#   { name: "Alex DeLarge", cohort: :november },
+#   { name: "The Wicked Witch of the West", cohort: :november },
+#   { name: "Terminator", cohort: :november },
+#   { name: "Freddy Krueger", cohort: :november },
+#   { name: "The Joker", cohort: :november },
+#   { name: "Joffrey Baratheon", cohort: :november },
+#   { name: "Norman Bates", cohort: :november },
+# ]
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish just hit return twice"
+
+  students = []
+
+  name = gets.chomp
+
+  while !name.empty?
+    students.push({ name: name, cohort: :november })
+    puts "now we have #{students.count} students"
+    name = gets.chomp
+  end
+
+  return students
+end
 
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(names)
-  names.each do |name|
-    puts name
+def print(students)
+  students.each do |students|
+    puts "#{students[:name]} (#{students[:cohort]} cohort)"
   end
 end
 
@@ -27,6 +44,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
